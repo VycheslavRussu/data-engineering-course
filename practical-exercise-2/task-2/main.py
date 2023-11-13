5df0e418ff20112c877631311519056ff49d4025
Vycheslav Russu <vycheslavrussu@gmail.com>
2023-11-13 11:55:37 +0500

    Небольшой апдейт по второму заданию

--- practical-exercise-2/task-2/main.py
+++ practical-exercise-2/task-2/main.py
@@ -14,7 +14,7 @@ for i in range(0, np.size(taskMatrix, 0)):
             y = np.append(y, j).astype(int)
             z = np.append(z, taskMatrix[i][j]).astype(int)
 
-np.savez('answer_2_var_65.npz', x, y, z)
-np.savez_compressed('compressed_answer_2_var_65.npz', x, y, z)
+np.savez('answer_2_var_65.npz', x=x, y=y, z=z)
+np.savez_compressed('compressed_answer_2_var_65.npz', x=x, y=y, z=z)
 
 # Размер файлов отличается почти в 10 раз
